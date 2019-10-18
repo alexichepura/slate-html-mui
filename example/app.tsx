afeeb54cfb2e1e7e9a37a7b779eb8b0c041efee1
Alexi Chepura <a.chepura@gmail.com>
2019-10-18 17:43:06 +0300

    initial: ValueJSON

--- example/app.tsx
+++ example/app.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { render } from "react-dom"
 import { SlateMuiEditor } from "../src/editor"
+import { initial } from "./initial"
 import { SlateMuiContext, SlateMui } from "../src/context"
 import { Value } from "slate"
 
-const value = Value.fromJSON({})
+const value = Value.fromJSON(initial)
 const slateMui = new SlateMui(value)
 
 render(

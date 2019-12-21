68fb24adf850eaf7dcb5db450d7f7aeccb912b2d
Alexi Chepura <a.chepura@gmail.com>
2019-12-21 20:20:21 +0300

    fix history

--- src/history.tsx
+++ src/history.tsx
@@ -2,31 +2,31 @@ import Redo from "@material-ui/icons/Redo"
 import Undo from "@material-ui/icons/Undo"
 import React, { FC } from "react"
 import { useSlate } from "slate-react"
-import { History } from "slate-history"
+import { History, HistoryEditor } from "slate-history"
 import { ToolbarButton } from "./toolbar-button"
 
 export const UndoButton: FC = () => {
-  const editor = useSlate()
+  const editor = (useSlate() as unknown) as HistoryEditor
   const history: History = editor.history
 
   const { undos } = history
   return (
     <ToolbarButton
       tooltipTitle="Undo"
-      onClick={() => editor.exec({ type: "undo" })}
+      onClick={() => editor.undo()}
       disabled={!undos || undos.length === 0}
       children={<Undo />}
     />
   )
 }
 export const RedoButton: FC = () => {
-  const editor = useSlate()
+  const editor = (useSlate() as unknown) as HistoryEditor
   const { history } = editor
   const { redos } = history
   return (
     <ToolbarButton
       tooltipTitle="Redo"
-      onClick={() => editor.exec({ type: "redo" })}
+      onClick={() => editor.redo()}
       disabled={!redos || redos.length === 0}
       children={<Redo />}
     />

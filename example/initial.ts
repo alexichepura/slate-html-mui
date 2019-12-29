643aeef3eae605a2630cd1d5c111574fc6de67c6
Alexi Chepura <a.chepura@gmail.com>
2019-12-29 11:15:01 +0300

    add link to initial

--- example/initial.ts
+++ example/initial.ts
@@ -9,4 +9,4 @@ export const initial: TTagElement[] = [
 ]
 
 export const initial_string =
-  "<p>This is editable <strong>rich</strong> text, <em>much</em> better than a <code>&lt;textarea&gt;</code>!</p><p>Since it&#39;s rich text, you can do things like turn a selection of text <strong>bold</strong>, or add a semantically rendered block quote in the middle of the page, like this:</p><blockquote>A wise quote.</blockquote><p>Try it out for yourself!</p>"
+  '<p>This is editable <strong>rich</strong> text, <em>much</em> <a href="https://www.slatejs.org" title="" target="_blank">better</a> than a <code>&lt;textarea&gt;</code>!</p><p>Since it&#39;s rich text, you can do things like turn a selection of text <strong>bold</strong>, or add a semantically rendered block quote in the middle of the page, like this:</p><blockquote>A wise quote.</blockquote><p>Try it out for yourself!</p>'

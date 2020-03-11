95fdf0b45a2350c315a586a819ab1bca0ffdd9a1
Alexi Chepura <a.chepura@gmail.com>
2020-03-11 16:43:22 +0300

    more tests

--- src/__tests__/html.tsx
+++ src/__tests__/html.tsx
@@ -1,4 +1,4 @@
-// import { shallow } from "enzyme"
+import renderer, { ReactTestRendererJSON, ReactTestRendererNode } from "react-test-renderer"
 import { Text } from "slate"
 import { createHtmlPlugin, EHtmlMark } from "../html"
 
@@ -8,7 +8,20 @@ const attributes: {
   "data-slate-leaf": true,
 }
 
+const createReactTestRendererJSON = (
+  type: string,
+  props: { [propName: string]: any },
+  children: null | ReactTestRendererNode[]
+): ReactTestRendererJSON => {
+  return { type, props, children }
+}
+
+const createLeafJson = (type: string, children: any): ReactTestRendererJSON => {
+  return createReactTestRendererJSON("span", attributes, [{ type, props: {}, children }])
+}
+
 const emptyText: Text = { text: "" }
+const testText = "testText"
 
 test("createHtmlPlugin should return null", () => {
   const plugin = createHtmlPlugin()
@@ -25,20 +38,54 @@ test("createHtmlPlugin should return null", () => {
 
 test("createHtmlPlugin should return single mark leaf", () => {
   const plugin = createHtmlPlugin()
-
   const result = plugin.RenderLeaf!({
     attributes,
-    children: [{ text: "1234asdf" } as Text],
     text: emptyText,
+    children: testText,
     leaf: { ...emptyText, [EHtmlMark.b]: true },
   })
-
   expect(result).not.toBeNull()
 
-  // console.log(result)
-  // const jsxElement = result!
-  // const shallowWrapper = shallow(jsxElement)
-  // console.log(shallowWrapper)
+  const component = renderer.create(result!)
+  const tree = component.toJSON()
+  const expected = createLeafJson(EHtmlMark.b, [testText])
+  expect(tree).toEqual(expected)
+})
 
-  // expect(shallowWrapper.html()).toEqual(`<span data-slate-leaf="true"><b></b></span>`)
+test("createHtmlPlugin should return 2 mark leafs", () => {
+  const plugin = createHtmlPlugin()
+  const result = plugin.RenderLeaf!({
+    attributes,
+    text: emptyText,
+    children: testText,
+    leaf: { ...emptyText, [EHtmlMark.b]: true, [EHtmlMark.u]: true },
+  })
+  expect(result).not.toBeNull()
+
+  const component = renderer.create(result!)
+  const tree = component.toJSON()
+  const expected = createLeafJson(EHtmlMark.u, [
+    createReactTestRendererJSON(EHtmlMark.b, {}, [testText]),
+  ])
+  expect(tree).toEqual(expected)
+})
+
+test("createHtmlPlugin should return 3 mark leafs", () => {
+  const plugin = createHtmlPlugin()
+  const result = plugin.RenderLeaf!({
+    attributes,
+    text: emptyText,
+    children: testText,
+    leaf: { ...emptyText, [EHtmlMark.b]: true, [EHtmlMark.u]: true, [EHtmlMark.em]: true },
+  })
+  expect(result).not.toBeNull()
+
+  const component = renderer.create(result!)
+  const tree = component.toJSON()
+  const expected = createLeafJson(EHtmlMark.u, [
+    createReactTestRendererJSON(EHtmlMark.em, {}, [
+      createReactTestRendererJSON(EHtmlMark.b, {}, [testText]),
+    ]),
+  ])
+  expect(tree).toEqual(expected)
 })

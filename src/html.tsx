1d322d4427912f74d66b8fe415d52fb7c6444bf8
Alex <braitmx@gmail.com>
2020-07-20 16:42:19 +0300

    update normalize function for block elements

--- src/html.tsx
+++ src/html.tsx
@@ -141,8 +141,12 @@ export const createHtmlPlugin = (): TSlatePlugin<TSlateTypeElement | any> => ({
 
     editor.normalizeNode = (entry) => {
       const [node, path] = entry
-      // If the element is a paragraph, ensure it's children are valid.
-      if (SlateElement.isElement(node) && isSlateTypeElement(node) && node.type === EHtmlBlock.p) {
+      // If the element is a block (except ul or ol), ensure it's children are valid.
+      if (SlateElement.isElement(node) && isSlateTypeElement(node) && isHtmlBlockElement(node)) {
+        if (node.type in EHtmlListTag) {
+          return
+        }
+
         for (const [child, childPath] of Node.children(editor, path)) {
           if (SlateElement.isElement(child) && !editor.isInline(child)) {
             Transforms.unwrapNodes(editor, { at: childPath })
